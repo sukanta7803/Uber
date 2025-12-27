1fd75ac4d4e548bda6ceb8ff302f8a011a4503a1
Sukanta Dey <setudeyindia@gmail.com>
2025-12-27 21:46:13 +0530

    Adding DataBase File

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,9 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDB = require('./DB/db');
 
+connectToDB();
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send('hello World');
