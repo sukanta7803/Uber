812a3cd2f9ce8355880e6aceee34d0a15d2e09c8
Sukanta Dey <setudeyindia@gmail.com>
2025-12-27 21:21:48 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+const http=require('http');
+const app =  require('./app');
+// console.log(process.env.PORT);
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+})
