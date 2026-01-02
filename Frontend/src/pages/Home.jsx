0ec1f84d759609e114cb3d453a2db71a66df3b8b
Sukanta Dey <setudeyindia@gmail.com>
2026-01-02 12:52:53 +0530

    add frontend again

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='h-screen flex justify-between flex w-full bg-red-400'>
+        <div className='bg-white'>
+            <h2>Get Started with Uber</h2>
+            <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
